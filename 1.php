994420d611da63dce41d81d92556c4d4544d0b50
anubhav.bhardwaj_cs18@gla.ac.in <Anubhav@8126>
2020-07-25 02:03:50 +0530

    Exercise 1 Complete

--- 1.php
+++ 1.php
@@ -16,19 +16,9 @@
 
 
 		<?php  
+        echo"<h1>Hello Anubhav</h1>"
 
-
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
-
-
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
-
-
-		   */
-
-		?>
+        ?>
 
 	
 
